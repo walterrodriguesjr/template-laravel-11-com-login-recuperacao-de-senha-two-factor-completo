24dfa08434e5c14cdd03b759a494da9aaabf5f56
Walter Rodrigues Junior <walterrjr.86@gmail.com>
2025-01-14 13:21:19 -0300

    1-criado e configurado o sidebar no padrao do adminlte3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import './bootstrap'; // Importação padrão do Laravel
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa os scripts do Bootstrap (incluindo Popper.js)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa os scripts do Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
+
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
 
